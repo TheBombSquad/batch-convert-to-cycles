d65b3801d0407f2fb6e8e26fe682bd024fc5db65
The BombSquad <35253266+TheBombSquad@users.noreply.github.com>
2020-05-30 15:29:49 -0700

    Added support for recursion

--- batch_convert_to_cycles.py
+++ batch_convert_to_cycles.py
@@ -6,17 +6,29 @@ import glob
 
 # CONFIG BEGINS HERE
 
-# Directory of Blend files to convert. Should not end in / or \. Script won't run if invalid
-blend_file_directory = "C://Path//To//Your//Blend//File/Folder"
+# Directory of Blend files to convert. Script won't run if invalid
+
+blend_file_directory = "C://Path//To//Your//Blend//File/Folder//"
 
 # File extension of the converted file. .blend will *REPLACE* the original copy -
 # if "Save Versions" in Blender preferences is less than 1. Otherwise, a '.blend1' of the original
 # will be saved. Can also be changed to something else to ensure nothing is overwritten.
+
 file_extension = ".blend"
+
+# Whether or not to recurse through the folder above. If you have all your Blender
+# files in seperate, individual folders, you want this to be "True". Otherwise, if you
+# only want to convert Blend files in a specific folder, and ignore sub-folders, you want
+# this to be "False".
+
+recurse = True
 
 # CONFIG ENDS HERE
 
-blend_files = glob.glob(blend_file_directory + "/*.blend")
+blend_files = []
+for file in glob.iglob(blend_file_directory + "**/*.blend", recursive=recurse):
+    blend_files.append(file)
+
 for file in blend_files:
     try:
         print("Opening: " + file)
